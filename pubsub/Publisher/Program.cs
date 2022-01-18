5fe7fe7fa155e629214e78199d4c44470f61e7e4
dylanbeattie <dylan@dylanbeattie.net>
2022-01-18 09:06:29 +0000

    Simple message publisher example

--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EasyNetQ;
+
+var bus = RabbitHutch.CreateBus("amqps://lxsmehro:u_0Wh1RbOEQthm_8VDcFdTe7JsiLLsTm@whale.rmq.cloudamqp.com/lxsmehro");
+
+while (true) {
+    Console.WriteLine("Press any key to publish a message");
+    Console.ReadKey(false);
+    var greeting = new Greeting();
+    bus.PubSub.Publish(greeting);
+    Console.WriteLine($"Published {greeting.Message}");
+}
+
+public class Greeting {
+    static int messageNumber = 0;
+    public string Message { get; set; }
+    public Greeting() {
+        Message = $"Greeting {messageNumber++} from {Environment.MachineName} at {DateTimeOffset.Now}";
+    }
+}
