a281d146f26703f503c55154504ffbfa4cbc4d25
dylanbeattie <dylan@dylanbeattie.net>
2022-01-18 09:30:45 +0000

    Working pubsub including simulated exception handling

--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -6,10 +6,11 @@ using Messages;
 var bus = RabbitHutch.CreateBus("amqps://lxsmehro:u_0Wh1RbOEQthm_8VDcFdTe7JsiLLsTm@whale.rmq.cloudamqp.com/lxsmehro");
 
 while (true) {
-    Console.WriteLine("Press any key to publish a message");
-    Console.ReadKey(false);
+    // Console.WriteLine("Press any key to publish a message");
+    // Console.ReadKey(false);
     var greeting = new Greeting();
     bus.PubSub.Publish(greeting);
     Console.WriteLine($"Published {greeting.Message}");
+    Thread.Sleep(TimeSpan.FromSeconds(1));
 }
 
--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -3,8 +3,19 @@ using Messages;
 
 var bus = RabbitHutch.CreateBus("amqps://lxsmehro:u_0Wh1RbOEQthm_8VDcFdTe7JsiLLsTm@whale.rmq.cloudamqp.com/lxsmehro");
 
-bus.PubSub.Subscribe<Greeting>("SUBSCRIBER_ID", g => {
-    Console.WriteLine($"Received {g.Message}");
+// Set subscriberId to something unique
+var subscriberId = Environment.MachineName;
+
+bus.PubSub.Subscribe<Greeting>(subscriberId, g => {
+    try {
+        if (g.Message.Contains("5 ")) throw new Exception("Simulated exception");
+        Console.WriteLine($"Received {g.Message}");
+    } catch (Exception ex) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"ERROR: {ex.Message}");
+        Console.ForegroundColor = ConsoleColor.White;
+        throw;
+    }
 });
 Console.WriteLine("Listening for messages... press Enter to Exit.");
 Console.ReadLine();
