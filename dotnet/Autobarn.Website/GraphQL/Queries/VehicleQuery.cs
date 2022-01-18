403e2d61dd15c30847fdbe93047f498e055bfcda
dylanbeattie <dylan@dylanbeattie.net>
2022-01-18 12:00:13 +0000

    Working GraphQL implementation as starting point for exercise

--- dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -17,6 +17,21 @@ namespace Autobarn.Website.GraphQL.Queries {
             Field<ListGraphType<VehicleGraphType>>("Vehicles", "Query to retrieve all Vehicles",
                 resolve: GetAllVehicles);
 
+            Field<ListGraphType<VehicleGraphType>>("VehiclesByColor", "Retrieve all vehicles of a particular color",
+                   new QueryArguments(MakeNonNullStringArgument("color", "The name of a color, eg 'blue', 'grey'")),
+                resolve: ListVehiclesByColor);
+        }
+
+        private object ListVehiclesByColor(IResolveFieldContext<object> context) {
+            var color = context.GetArgument<string>("color");
+            return db.ListVehicles()
+                .Where(v => v.Color.Contains(color, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private QueryArgument MakeNonNullStringArgument(string name, string description) {
+            return new QueryArgument<NonNullGraphType<StringGraphType>> {
+                Name = name, Description = description
+            };
         }
 
         private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> arg) {
